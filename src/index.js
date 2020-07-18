0366e7628efb2dce20de40e224bdd7fba46ad4e9
aSilas <silastj@hotmail.com>
2020-07-18 16:45:46 -0300

    Conceitos Básicos

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Title from './title';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App  />
+    <Title name="Amós Silas Santos"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+class Title extends React.Component {
+    render() {
+    return<h1>Novo {this.props.name}</h1>
+    }
+}
+  
+  export default Title;
